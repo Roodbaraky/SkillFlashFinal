be3eb8ad7d6de17b5391bdf97e823ae0a6b19663
anaggomes <157487724+anaggomes@users.noreply.github.com>
2024-05-20 10:29:25 +0100

    writes log in component

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export default function Index() {
+  return;
+}
---
+++ components/Login.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, TextInput, Pressable, StyleSheet } from "react-native";
+
+export default function Login() {
+  const [usernameInput, setUsernameInput] = React.useState("");
+  const [passwordInput, setPasswordInput] = React.useState("");
+
+  function checkUserExists() {
+    console.log("user exists!");
+  }
+
+  return (
+    <SafeAreaView>
+      <Text style={styles.title}>Log in page</Text>
+      <Text style={styles.label}>Username</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsernameInput}
+        value={usernameInput}
+        placeholder="username"
+      />
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPasswordInput}
+        value={passwordInput}
+        placeholder="password"
+        textContentType="password"
+      />
+      <Pressable onPress={checkUserExists}>
+        <Text style={styles.button}>Log in</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  logInContainer: {
+    flex: 1,
+    paddingTop: 58,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 50,
+    color: "black",
+    marginTop: 100,
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 250,
+  },
+  label: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 15,
+    marginBottom: 5,
+  },
+  button: {
+    alignSelf: "center",
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
