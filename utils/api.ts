cafa3b8f6d788a61cbc109981c9c19121686889a
湾仔 <Wendy@shiwanwandembp.home>
2024-05-22 12:26:24 +0100

    DeckTile and home decklist

--- utils/api.ts
+++ utils/api.ts
@@ -1,45 +1,72 @@
 import axios from "axios";
+import { HomeDeck } from "./utils";
 
 export async function createUser(
-  username: string,
-  password: string,
-  email: string
+	username: string,
+	password: string,
+	email: string
 ) {
-  const signupBody = { username, password, email };
-  try {
-    const { data } = await axios.post(
-      "https://skillflashbackend.onrender.com/api/users/signup",
-      signupBody
-    );
-    return data.user;
-  } catch (err) {
-    console.log(err); //do we need error handling? here?
-  }
+	const signupBody = { username, password, email };
+	try {
+		const { data } = await axios.post(
+			"https://skillflashbackend.onrender.com/api/users/signup",
+			signupBody
+		);
+		return data.user;
+	} catch (err) {
+		console.log(err); //do we need error handling? here?
+	}
 }
 
 export async function checkUserExists(username: string, password: string) {
-  const loginBody = { username, password };
-  try {
-    const { data } = await axios.post(
-      "https://skillflashbackend.onrender.com/api/users/login",
-      loginBody
-    );
+	const loginBody = { username, password };
+	try {
+		const { data } = await axios.post(
+			"https://skillflashbackend.onrender.com/api/users/login",
+			loginBody
+		);
 
-    return data.user;
-  } catch (err) {
-    return err;
-    //do we need error handling? here?
-  }
+		return data.user;
+	} catch (err) {
+		return err;
+		//do we need error handling? here?
+	}
 }
 
 export async function doesUserExist(usernameInput: string) {
-  try {
-    const { data } = await axios.post(
-      `https://skillflashbackend.onrender.com/api/users/${usernameInput}`
-    );
-    console.log(data);
-    return true;
-  } catch (err) {
-    console.log(err); //do we need error handling? here?
-  }
+	try {
+		const { data } = await axios.post(
+			`https://skillflashbackend.onrender.com/api/users/${usernameInput}`
+		);
+		console.log(data);
+		return true;
+	} catch (err) {
+		console.log(err); //do we need error handling? here?
+	}
 }
+export async function getDecksByUsername(username: string) {
+	try {
+		// const { data } = await axios.post(
+		// 	`https://skillflashbackend.onrender.com/api/decks/${username}`
+		// );
+		// console.log(data);
+		return testDecks;
+	} catch (err) {
+		console.log(err); //do we need error handling? here?
+	}
+}
+
+const testDecks: HomeDeck[] = [
+	{
+		deckId: "deck1",
+		deckName: "string",
+		tags: ["javascript", "react", "node"],
+		totalCards: 6,
+	},
+	{
+		deckId: "342534543",
+		deckName: "deck2",
+		tags: ["javascript", "react", "node"],
+		totalCards: 12,
+	},
+];
