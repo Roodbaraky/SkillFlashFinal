4e75f483bda073072b0612f487afed9298ae1780
Koo <kooroshr@hotmail.com>
2024-05-25 00:40:07 +0100

    linked & added accessibility buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text} from "react-native";
+
 import { SafeAreaView } from "react-native-safe-area-context";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
@@ -26,15 +27,17 @@ export default function Index() {
 	}
 
 	return (
-		<SafeAreaView style={styles.indexContainer}>
-			<Text style={styles.title}>SkillFlash</Text>
-			<Pressable onPress={() => handlePress("login")}>
-				<Text style={styles.button}>Log in</Text>
-			</Pressable>
-			<Pressable onPress={() => handlePress("signup")}>
-				<Text style={styles.button}>Sign up</Text>
-			</Pressable>
-		</SafeAreaView>
+		
+			<SafeAreaView style={styles.indexContainer}>
+				<Text style={styles.title}>SkillFlash</Text>
+				<Pressable onPress={() => handlePress("login")}>
+					<Text style={styles.button}>Log in</Text>
+				</Pressable>
+				<Pressable onPress={() => handlePress("signup")}>
+					<Text style={styles.button}>Sign up</Text>
+				</Pressable>
+			</SafeAreaView>
+	
 	);
 }
 
