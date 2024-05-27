d95046a985492c97c38c8b448776fef2be49da7b
湾仔 <Wendy@shiwanwandembp.home>
2024-05-27 01:37:28 +0100

    added loading and error component

--- app/deck/[id]/_layout.tsx
+++ app/deck/[id]/_layout.tsx
@@ -14,9 +14,14 @@ export default function HomeLayout() {
 
 	return (
 		<Stack>
-			{/* <Stack.Screen name="userPage" options={{ headerShown: false }} /> */}
-			<Stack.Screen name="deck/[id]" options={{ headerShown: false }} />
-			<Stack.Screen name="play" options={{ headerShown: false, navigationBarHidden:true	}} />
+			<Stack.Screen
+				name="index"
+				options={{ headerShown: false, headerBackTitle: "Back" }}
+			/>
+			<Stack.Screen
+				name="play"
+				options={{ headerShown: false, navigationBarHidden: true }}
+			/>
 		</Stack>
 	);
 }
